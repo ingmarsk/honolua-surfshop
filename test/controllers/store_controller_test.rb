da693b5f79f6ce5c7813d089975bfb219a31e720
Ingemar Andrade <i.andrade@volcanic.co.uk>
2017-08-06 15:37:05 +0100

    Implement fragment caching for products

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
+
+	# Assertions are based on the test data from Fixtures
   test "should get index" do
     get :index
     assert_response :success
+    assert_select 'h3', 'SexWax'
+    assert_select 'h4 span', /\$[,\d]+\.\d\d/
   end
 
 end
