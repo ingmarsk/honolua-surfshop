21f4c37f5b1edda963f77417616430f5419f61f3
Ingemar Andrade <i.andrade@volcanic.co.uk>
2017-08-21 19:20:46 +0100

    Apply down method to rollback 20170821174333 migration

--- db/migrate/20170821174333_combine_items_in_cart.rb
+++ db/migrate/20170821174333_combine_items_in_cart.rb
@@ -27,4 +27,16 @@ class CombineItemsInCart < ActiveRecord::Migration
   	end
   end
 
+  # This makes the changes from up() reversible with a migration rollback
+  # Find line items with a quantity > 1, adds new line items for this cart and product each with a quantity = 1
+  # Deletes the original line item
+  def down
+  	LineItem.where("quantity > 1").each do |line_item|
+  		line_item.quantity.times do 
+  			LineItem.create cart_id: line_item.cart_id, product_id: line_item.product_id, quantity: 1
+  		end
+  		line_item.destroy
+  	end
+  end
+
 end
