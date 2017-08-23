ced69e3f8c5c2a17004d3fa35ce0993fe4f44a18
Ingemar Andrade <i.andrade@volcanic.co.uk>
2017-08-23 21:33:02 +0100

    Show the cart on index page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,12 @@
 class StoreController < ApplicationController
+
+	# Enables us to use @cart in the store layout from the StoreController
+	include CurrentCart	
+	before_action :set_cart
+
   def index
   	# Get all products from db ordered by title
   	@products = Product.order(:title)
   end
+
 end
