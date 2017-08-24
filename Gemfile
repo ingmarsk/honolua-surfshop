5f15f7d711c6f7d94501a220ae4827383d2aec53
Ingemar Andrade <i.andrade@volcanic.co.uk>
2017-08-24 23:58:19 +0100

    Use jQuey UI to highlight cart changes

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 
+# Use the jQuery UI library
+gem 'jquery-ui-rails'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -41,7 +41,10 @@ class LineItemsController < ApplicationController
         format.html { redirect_to store_url, notice: 'Line item successfully created' }
         # Do not redirect to the index if request comes from JavaScript
         # When create() finishes with the Ajax request, Rails looks for a "create" template to render
-        format.js  
+        # Identify the most recently updated item in the cart by passing the current line item to the template
+        #   by the @line_item instance variable
+        # format.js 
+        format.js { @current_item = @line_item }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
