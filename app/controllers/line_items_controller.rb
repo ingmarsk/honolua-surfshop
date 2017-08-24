a599169ee5f60be0f16741d577a53eafebed3817
Ingemar Andrade <i.andrade@volcanic.co.uk>
2017-08-24 22:54:11 +0100

    Use an Ajax request to update the Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -38,7 +38,10 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item successfully created' }
+        format.html { redirect_to store_url, notice: 'Line item successfully created' }
+        # Do not redirect to the index if request comes from JavaScript
+        # When create() finishes with the Ajax request, Rails looks for a "create" template to render
+        format.js  
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
