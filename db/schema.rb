a5b875662c07736a4111e33dbdb724c4412ba87f
Ingemar Andrade <i.andrade@volcanic.co.uk>
2017-09-08 22:10:25 +0100

    Add more columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828023717) do
+ActiveRecord::Schema.define(version: 20170908210431) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 20170828023717) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
   end
 
 end
